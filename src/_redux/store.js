25538183411429bb6f82861b8fa7c9cdeb9c3fc2
Shamal <shamal4dev@outlook.com>
2020-08-24 22:42:56 +0530

    removed logger in production

--- src/_redux/store.js
+++ src/_redux/store.js
@@ -3,12 +3,15 @@ import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
 
-const loggerMiddleware = createLogger();
+let middleWares = [thunkMiddleware];
+if (process.env.NODE_ENV !== 'production') {
+    const loggerMiddleware = createLogger();
+    middleWares = [...middleWares, loggerMiddleware]
+}
 
 export const store = createStore(
     rootReducer,
     applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
+       middleWares
     )
 );
