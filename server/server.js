811ca872198e6ffaf5f07c594a9dc4f5914897c0
Shamal <shamal4dev@outlook.com>
2020-09-04 21:16:45 +0530

    removed json part because we cant run both json and express server in production

--- server/server.js
+++ server/server.js
@@ -1,25 +1,15 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('server/db.json');
-const middlewares = jsonServer.defaults();
 const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'build');
-const dbPort = process.env.PORT +'/api' || 4000;
-const appPort = process.env.PORT || 3000;
-server.use(middlewares);
-server.use(router);
+const port = process.env.PORT || 3000;
 
-server.listen(dbPort);
 
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
-app.listen(appPort, err => {
+app.listen(port, err => {
     if(err) throw err;
    console.log('Server is up!');
 });
-
-
