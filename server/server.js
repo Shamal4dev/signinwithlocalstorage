343b811054847808ed8c9e085d758961ce55d5c7
Shamal <shamal4dev@outlook.com>
2020-09-04 20:32:11 +0530

    Added json file as database

--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('server/db.json');
+const middlewares = jsonServer.defaults();
 const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'build');
-const port = process.env.PORT || 3000;
+const dbPort = process.env.PORT || 4000;
+const appPort = process.env.PORT || 3000;
+server.use(middlewares);
+server.use(router);
 
+server.listen(dbPort);
 
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
-app.listen(port, err => {
+app.listen(appPort, err => {
     if(err) throw err;
    console.log('Server is up!');
 });
+
+
